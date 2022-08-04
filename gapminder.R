1178e199ee0a545396a24638030529d129573d81
frantz93 <frantzroby@gmail.com>
2022-08-04 17:59:19 -0400

    code added

--- gapminder.R
+++ gapminder.R
@@ -22,3 +22,20 @@ g %>% filter(year %in% c(1962, 1970, 1980, 1990, 2012) & continent %in% c('Europ
 
 g %>% filter(year %in% c(1962, 2012)) %>% ggplot(aes(fertility, life_expectancy, color=continent)) + geom_point() +
   facet_grid(continent~year)
+
+summary(g['country'], maxsum = 1000)
+countries <- c('South Korea', 'Germany')
+g %>% filter(country %in% countries) %>% ggplot(aes(year, life_expectancy, col=country)) + geom_line()
+
+labels <- data.frame(country = countries, x = c(1975, 1965), y = c(60, 72))
+g %>% filter(country %in% countries) %>% ggplot(aes(year, life_expectancy, color=country)) + geom_line() + 
+  geom_text(data = labels, aes(x, y, label=country)) + theme(legend.position = 'none')
+
+g <- g %>% mutate(dpd = gdp/population/365)
+g <- g %>% mutate(log2_dpd = log2(dpd))
+head(g, 10)
+g %>% filter(year==1970 & !is.na(log2_dpd)) %>% ggplot(aes(log2_dpd)) + geom_histogram
+
+g %>% filter(year==1970 & !is.na(dpd)) %>% ggplot(aes(dpd)) + geom_histogram(binwidth = 1, color = 'black') + 
+  scale_x_continuous(trans = 'log2')
+
