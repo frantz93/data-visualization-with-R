dfc3eb0411770115865c59c89420011faa847a42
frantz93 <frantzroby@gmail.com>
2022-08-05 21:00:24 -0400

    code lines added

--- gapminder.R
+++ gapminder.R
@@ -36,7 +36,7 @@ g %>% filter(country %in% countries) %>% ggplot(aes(year, life_expectancy, color
 #Log Transformation
 g <- g %>% mutate(dpd = gdp/population/365)
 g <- g %>% mutate(log2_dpd = log2(dpd))
-head(g, 10)
+head(g)
     #Log Value transformation
 g %>% filter(year==1970 & !is.na(log2_dpd)) %>% ggplot(aes(log2_dpd)) + geom_histogram(binwidth = 1, color = 'black')
     #Log Scale transformation
@@ -79,9 +79,80 @@ p4 + geom_histogram(binwidth = 1, col = 'black') + scale_x_continuous(trans = 'l
 p5 <- g %>% filter(year %in% c(past_year, present_year) & !is.na(dpd)) %>% mutate(region = reorder(region, dpd, FUN=median)) %>%
   ggplot(aes(region, dpd)) + theme(axis.text.x = element_text(angle=90, hjust = 1)) +
   scale_y_continuous(trans = 'log2') + xlab("")
-
 p5 + geom_boxplot(aes(fill=continent)) + facet_grid(year~.) + scale_fill_discrete(name='Continent')  #by faceting graphs
 p5 + geom_boxplot(aes(fill=factor(year))) + scale_fill_discrete(name='Year') #on same graph
 
+
 #Density Plots
+
+#first, let's built a list of countries which data exist in both 1970 and 2010 years
+list1 <- g %>% filter(year==1970 & !is.na(dpd)) %>% .$country
+list2 <- g %>% filter(year==2010 & !is.na(dpd)) %>% .$country
+countries <- intersect(list1, list2)
+
+years <- c(past_year, present_year)
+p6 <- g %>% filter(year %in% years & country %in% countries) %>% ggplot(aes(dpd))
+#a faceted smooth density plots - area under each curves adds to 1
+p6 + facet_grid(year~.) + geom_density(alpha = 0.5, bw = 0.75, fill='blue') + scale_x_continuous(trans = 'log2')
+
+#note we have 174 developing countries and 42 developed countries
+g %>% filter(year %in% years & country %in% countries) %>% 
+  mutate(group = if_else(region %in% west, 'West', 'Developing')) %>% group_by(group) %>% summarise(n=n()) %>% 
+  knitr::kable()
+#we can facet the density of the two distributions (the two groups) together for each year
+p7 <- g %>% filter(year %in% years & country %in% countries) %>% 
+  mutate(group = if_else(region %in% west, 'West', 'Developing')) %>% ggplot(aes(dpd))
+p7 + geom_density(alpha = 0.2, aes(fill=group)) + facet_grid(year~.) + scale_x_continuous(trans = 'log2')   #plot the density in the y axis
+p8 <- g %>% filter(year %in% years & country %in% countries) %>% 
+  mutate(group = if_else(region %in% west, 'West', 'Developing')) %>% ggplot(aes(x = dpd, y = ..count..))
+p8 + geom_density(alpha = 0.2, bw = 0.75, aes(fill=group)) + facet_grid(year~.) + scale_x_continuous(trans = 'log2')   #plot the numbers in the y axis
+
+g <- g %>% mutate(group = case_when(
+  .$region %in% west ~ 'West',
+  .$region %in% c("Eastern Asia", "South-Eastern Asia") ~ 'East Asia',
+  .$region %in% c("Caribbean", "Central America", "South America") ~ 'Latin America',
+  .$continent == "Africa" & .$region != "Northern Africa" ~ 'Sub-Saharan Africa',
+  TRUE ~ 'Others'))
+
+g$group <- factor(g$group, levels = c('Others', 'Latin America', 'East Asia', 'Sub-Saharan Africa', 'West'))
+
+p9 <- g %>% filter(year %in% years & country %in% countries) %>% ggplot(aes(x = dpd, y = ..count..))
+p9 + geom_density(alpha = 0.2, bw = 0.75, aes(fill=group), position = 'stack') + facet_grid(year~.) + scale_x_continuous(trans = 'log2') 
+
+#setting a weight regarding population size
+g <- g %>% filter(year %in% years & country %in% countries) %>% group_by(year) %>% mutate(weight = population/sum(population*2)) %>% ungroup()
+
+#new graph
+p10 <- g %>% filter(year %in% years & country %in% countries) %>% ggplot(aes(dpd, weight = weight))
+p10 + geom_density(alpha = 0.2, bw = 0.75, aes(fill=group), position = 'stack') + facet_grid(year~.) + scale_x_continuous(trans = 'log2')
+
+g %>% filter(year %in% years & country %in% countries) %>% group_by(year) %>% mutate(weight = population/sum(population*2)) %>% ungroup() %>% 
+  ggplot(aes(dpd, fill = group, weight = weight)) + scale_x_continuous(trans = 'log2') + geom_density(alpha = 0.2, bw = 0.75, position = 'stack') +
+  facet_grid(year~.)
+
+
+#Ecological fallacy
+g <- g %>% mutate(group = case_when(
+  .$region %in% west ~ 'The West',
+  .$region %in% 'Northern Africa' ~ 'Northern Africa',
+  .$region %in% c('Eastern Asia', 'South-Eastern Asia') ~ 'East Asia',
+  .$region %in% 'Southern Asia' ~ 'Southern Asia',
+  .$region %in% c('Caribbean', 'Central America', 'South America') ~ 'Latin America',
+  .$continent == 'Africa' & .$region != 'Northern Africa' ~ 'Sub-Saharan Africa',
+  .$region %in% c("Melanesia", "Micronesia", "Polynesia") ~ "Pacific Islands"))
+    
+    # define a data frame with group average income and average infant survival rate
+surv_income <- g %>%
+  filter(year %in% present_year & !is.na(gdp) & !is.na(infant_mortality) & !is.na(group)) %>%
+  group_by(group) %>%
+  summarize(income = sum(gdp)/sum(population)/365,
+            infant_survival_rate = 1 - sum(infant_mortality/1000*population)/sum(population))
+surv_income %>% arrange(income)
+
+    # plot infant survival versus income, with transformed axes
+surv_income %>% ggplot(aes(income, infant_survival_rate, label = group, color = group)) +
+  scale_x_continuous(trans = "log2", limit = c(0.25, 150)) +
+  scale_y_continuous(trans = "logit", limit = c(0.875, .9981),
+                     breaks = c(.85, .90, .95, .99, .995, .998)) +
+  geom_label(size = 3, show.legend = FALSE) 
 
