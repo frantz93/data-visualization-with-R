1483d4962442aa305554e137eb57a5712f4e0414
frantz93 <frantzroby@gmail.com>
2022-08-12 23:09:29 -0400

    added code

--- final_assessment_part2.R
+++ final_assessment_part2.R
@@ -26,3 +26,52 @@ tc$carbon_emissions[tc$year==last_year]/tc$carbon_emissions[tc$year==first_year]
 tc %>% filter(year %in% c(first_year, last_year)) %>% summarise(ratio = carbon_emissions[1]/carbon_emissions[2])
 
 head(tc)
+first_year <- tc %>% filter(!is.na(temp_anomaly)) %>% pull(year) %>% min()
+last_year <- tc %>% filter(!is.na(temp_anomaly)) %>% pull(year) %>% max()
+tc$temp_anomaly[tc$year==last_year] - tc$temp_anomaly[tc$year==first_year]
+
+p <- tc %>% filter(!is.na(temp_anomaly)) %>% ggplot(aes(year, temp_anomaly)) + geom_line(col='red')
+p + geom_hline(yintercept = 0, col='blue')
+
+p + labs(y='Temperature anomaly (degrees C)', title='Temperature anomaly relative to 20th century mean, 1880-2018') + 
+  geom_text(aes(x=2000, y=0.05),label='20th century mean', col='blue') + geom_hline(yintercept = 0, col='blue')
+
+p2 <- tc %>% filter(!is.na(temp_anomaly) & !is.na(land_anomaly) & !is.na(ocean_anomaly)) %>% ggplot() + 
+  geom_line(size=1, aes(year, temp_anomaly), col='black') + geom_line(size=1, aes(year, land_anomaly), col='red') + 
+  geom_line(size=1, aes(year, ocean_anomaly), col='blue')
+p2 + labs(y='Temperature anomaly (degrees C)', title='Temperature anomaly relative to 20th century mean, 1880-2018') +
+  geom_hline(yintercept = 0, col='green') + geom_text(aes(x=2005, y=0.05),label='20th century mean', col='darkgrey') +
+  geom_text(aes(x=2020, y=1.4),label='Land', col='red') + geom_text(aes(x=2020, y=0.9),label='Global', col='black') +
+  geom_text(aes(x=2020, y=0.7),label='Ocean', col='blue')
+  theme(legend.position = 'right')
+p2 + theme_void()
+
+  
+#about the greenhouse gases
+head(gh)
+summary(as.factor(gh$gas))
+
+gh %>%
+  ggplot(aes(year, concentration, col=gas)) +
+  geom_line() +
+  facet_grid(gas~., scales = "free") +
+  geom_vline(xintercept = 1850) +
+  ylab("Concentration (ch4/n2o ppb, co2 ppm)") +
+  ggtitle("Atmospheric greenhouse gas concentration by year, 0-2000")
+
+tc %>% filter(!is.na(carbon_emissions)) %>% ggplot(aes(year, carbon_emissions)) + geom_line() + geom_vline(xintercept = 1850, col='red') +
+  geom_vline(xintercept = 1960, col='green') + geom_vline(xintercept = 2010, col='blue')
+
+#about the global co2
+head(hc)
+summary(hc)
+summary(as.factor(hc$source))
+
+hc %>% ggplot(aes(year, co2, col=source)) + geom_line()
+hc %>% ggplot(aes(year, co2, col=source)) + geom_line() + scale_x_continuous(trans = 'sqrt')
+co2_time <- hc %>% ggplot(aes(year, co2, col=source)) + geom_line()
+
+co2_time + xlim(-800000, -775000)
+co2_time + xlim(-375000, -330000)
+co2_time + xlim(-140000, -120000)
+co2_time + xlim(-3000, 2018)
--- titanic.R
+++ titanic.R
@@ -1,7 +1,9 @@
 #----------------Titanic Assessment ---------------------#
+getwd()
 options(digits = 3)
 library(tidyverse)
 library(titanic)
+library(descr)
 
 t <- titanic_train %>%
   select(Survived, Pclass, Sex, Age, SibSp, Parch, Fare) %>%
@@ -37,3 +39,54 @@ t %>% filter(!is.na(Survived) & !is.na(Sex)) %>% ggplot(aes(Survived, fill = Sex
 t %>% filter(!is.na(Survived)) %>% summarise(mean = mean(Survived==1))
 typeof(t$Survived)
 mean(t$Survived==1)
+
+#making a density plot of age filled by survival status
+t %>% filter(!is.na(Age) & !is.na(Survived)) %>% ggplot(aes(Age, ..count.., fill=Survived)) + geom_density(alpha=0.2)
+summary(as.factor(t$Age))
+t2 <- t %>% filter(!is.na(Age) & !is.na(Survived)) %>% mutate(age_range = case_when(
+  .$Age <= 8 ~ ']0 - 8]',
+  .$Age <= 10 ~ ']8 - 10]',
+  .$Age <= 18 ~ ']10 - 18]',
+  .$Age <= 30 ~ ']18 - 30]',
+  .$Age <= 50 ~ ']30 - 50]',
+  .$Age <= 70 ~ ']50 - 70]',
+  .$Age <= 80 ~ ']70 - 80]',
+)) %>% select(Survived, age_range)
+
+t2 <- t %>% filter(!is.na(Age) & !is.na(Survived)) %>% mutate(age_range = case_when(
+  .$Age < 8 ~ '[0 - 8[',
+  .$Age < 10 ~ '[8 - 10[',
+  .$Age < 18 ~ '[10 - 18[',
+  .$Age < 30 ~ '[18 - 30[',
+  .$Age < 50 ~ '[30 - 50[',
+  .$Age < 70 ~ '[50 - 70[',
+  .$Age <= 80 ~ '[70 - 80]',
+)) %>% select(Survived, age_range)
+
+crosstab(t2$age_range, t2$Survived)
+CrossTable(t2$age_range, t2$Survived)
+tab = CrossTable(t2$age_range, t2$Survived, prop.r = TRUE, prop.c = FALSE, prop.t = FALSE,
+                 prop.chisq = FALSE, chisq = FALSE)
+max(tab$prop.row)
+
+#assessing survival status vs paid fares
+t %>% filter(!is.na(Fare) & !is.na(Survived)) %>% ggplot(aes(x=Survived, y=Fare, fill = Survived)) + 
+  geom_boxplot(width=0.5, alpha=0.5) + geom_jitter(width = 0.1, alpha = 0.2) + scale_y_continuous(trans = 'log2')
+#making bar plots of passenger class vs survival status
+t %>% filter(!is.na(Survived) & !is.na(Pclass)) %>% ggplot(aes(Pclass, fill = Survived)) + geom_bar(position = 'dodge2')
+t %>% filter(!is.na(Survived) & !is.na(Pclass)) %>% ggplot(aes(Pclass, fill = Survived)) + geom_bar(position = 'fill')
+t %>% filter(!is.na(Survived) & !is.na(Pclass)) %>% ggplot(aes(Survived, fill = Pclass)) + geom_bar(position = 'dodge2')
+t %>% filter(!is.na(Survived) & !is.na(Pclass)) %>% ggplot(aes(Survived, fill = Pclass)) + geom_bar(position = 'fill')
+t %>% filter(!is.na(Survived) & !is.na(Pclass)) %>% ggplot(aes(Pclass, fill = Survived)) + geom_bar(position = 'stack')
+
+#png(filename = 'pic.png')    #try this command to save a plot in a specific file
+t %>% filter(!is.na(Survived)) %>% summarise(status=c('survived', 'dead'), proportion = c(mean(Survived==1), dead = mean(Survived==0))) %>% 
+  ggplot(aes(status,proportion,  fill = status)) + geom_bar(stat = 'identity') + scale_y_continuous(label = scales::percent_format()) +
+  labs(title = 'Reported survival rates for Titanic passengers', caption = '©FRPDJ \n  Source: dslab') + theme(legend.position = 'none')
+#dev.off()
+
+#creating a density plot of age filled by survival status and faceted by sex and passenger class
+t %>% filter(!is.na(Age) & !is.na(Sex) & !is.na(Pclass)) %>% ggplot(aes(Age, ..count.., fill = Survived)) + geom_density(alpha=0.3) + 
+  facet_grid(Pclass~Sex)
+t %>% filter(!is.na(Age) & !is.na(Sex) & !is.na(Pclass)) %>% ggplot(aes(Age, ..density.., fill = Sex)) + geom_density(alpha=0.3) + 
+  facet_grid(Pclass~.)
